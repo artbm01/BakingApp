b8b02e9367ae53579d3919c02cda2b397129ee6c
artbm01 <a.bredeekmendes@gmail.com>
2018-07-20 17:06:43 -0700

    App with exoplayer working

--- app/src/main/java/com/example/bakingapp/ui/StepActivity.java
+++ app/src/main/java/com/example/bakingapp/ui/StepActivity.java
@@ -1,11 +1,23 @@
 package com.example.bakingapp.ui;
 
+import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.bakingapp.R;
 import com.example.bakingapp.models.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 
 import java.util.ArrayList;
 
@@ -13,6 +25,7 @@ public class StepActivity extends AppCompatActivity {
 
     private ArrayList<Step> stepsList;
     int id;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,4 +42,6 @@ public class StepActivity extends AppCompatActivity {
         fragmentManager.beginTransaction().add(R.id.step_fragment, fragment).commit();
 
     }
+
+
 }
--- app/src/main/java/com/example/bakingapp/ui/StepFragment.java
+++ app/src/main/java/com/example/bakingapp/ui/StepFragment.java
@@ -1,5 +1,6 @@
 package com.example.bakingapp.ui;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,10 +10,22 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.bakingapp.R;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 
 public class StepFragment extends Fragment{
 
     String descriptionText;
+    long playbackPosition = 0;
+    int currentWindow = 0;
+    boolean playWhenReady = true;
 
     public StepFragment(){}
 
@@ -24,12 +37,33 @@ public class StepFragment extends Fragment{
 
         final TextView description = rootView.findViewById(R.id.step_description_tv);
         description.setText(descriptionText);
-
+        final PlayerView playerView = rootView.findViewById(R.id.player_view);
+        startPlayer(playerView);
 
         return rootView;
     }
 
     public void setStep(String descriptionText){
         this.descriptionText = descriptionText;
+    }
+
+    public void startPlayer(PlayerView playerView){
+        SimpleExoPlayer player;
+        player = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(getContext()),
+                new DefaultTrackSelector(), new DefaultLoadControl());
+
+        playerView.setPlayer(player);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        Uri uri = Uri.parse("http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4");
+        MediaSource mediaSource = buildMediaSource(uri);
+        player.prepare(mediaSource, true, false);
+    }
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("exoplayer-codelab")).
+                createMediaSource(uri);
     }
 }
